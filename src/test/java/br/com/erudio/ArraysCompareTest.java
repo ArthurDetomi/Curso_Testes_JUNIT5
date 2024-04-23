f0bb4e167b2834a2e5b7a2d7cec29cc5cf807a3f
Arthur Detomi <arthurdetomi@gmail.com>
2024-04-23 20:48:05 -0300

    Testando o tempo de métodos com JUnit

--- src/test/java/br/com/erudio/ArraysCompareTest.java
+++ src/test/java/br/com/erudio/ArraysCompareTest.java
@@ -1,8 +1,10 @@
 package br.com.erudio;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
@@ -17,5 +19,18 @@ public class ArraysCompareTest {
 
         assertArrayEquals(numbers, expected);
     }
+
+    @Test
+    //@Timeout(1)
+    @Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+    void testSortPerformance() {
+        int[] numbers = new int[]{10, 1, 8, 7, 6, 3, 4, 5, 2, 9};
+        for (int i = 0; i < 1000000000; i++) {
+            numbers[0] = i;
+            Arrays.sort(numbers);
+        }
+    }
+
+
 
 }
